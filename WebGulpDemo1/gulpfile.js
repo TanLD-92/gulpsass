fc3d8ef1a811a097c160e9aa839a641c27d30db9
tanld <tanld@greenglobal.vn>
2016-03-24 01:12:39 +0700

    up code

---
+++ WebGulpDemo1/gulpfile.js
@@ -0,0 +1,22 @@
+﻿var gulp = require('gulp');
+var sass = require('gulp-sass');
+var livereload = require('gulp-livereload');
+var browserSync = require("browser-sync").create();
+
+gulp.task('sass-compile', function () {
+    gulp.src(['./Styles/scss/**/*.scss', './Styles/scss/*.scss'])
+        .pipe(sass())
+        .pipe(gulp.dest('./Content/'))
+        .pipe(livereload({ start: true }));
+});
+
+gulp.task('watch-sass', function () {
+    gulp.watch(['./Styles/scss/**/*.scss', './Styles/scss/*.scss'], ['sass-compile']);
+    livereload.listen();
+});
+gulp.task('browser-sync', function () {
+    browserSync.init({
+        proxy: "http://localhost:59612/"
+    });
+});
+gulp.task('default', ['sass-compile', 'watch-sass', 'browser-sync']);
